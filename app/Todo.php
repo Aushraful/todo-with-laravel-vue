519037dad197983abf8058065c2e73dddb18417f
Aushraful <aushraful@gmail.com>
2020-01-29 20:41:03 +0600

    model updated

--- app/Todo.php
+++ app/Todo.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Todo extends Model
 {
-    //
+    protected $fillable = [
+        'title','completed',
+    ];
+    public $hidden = ['created_at','updated_at'];
+    public $table = 'todos';
 }
